2b69257b02818d90b4fae446e703a2be5fe1dce3
DonOutcast <lymondgl@student.21-school.ru>
2023-02-17 11:42:42 +0300

    need search all nemes of measures

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 url_measures = "https://myrosmol.ru/measures"
-
+url_test = "https://myrosmol.ru/measures/2"
 
 def check_status_code(status_code: int) -> bool:
     """
@@ -23,9 +23,14 @@ def get_measures(url: str) -> list[str]:
     :type: :obj: `str`
     """
     response = requests.get(url)
+    # print(response.status_code)
     if check_status_code(response.status_code):
-        print(response.content)
+        soup = BeautifulSoup(response.content, "html.parser")
+        print(soup.prettify())
 
+        temp = soup.find_all("div", {"class": "col-md-6 d-flex"})
+        for i in temp:
+            print(i)
 
 
 def start_event_loop() -> None:
@@ -34,7 +39,7 @@ def start_event_loop() -> None:
     :return: None
     :type: :obj: `None`
     """
-    get_measures(url_measures)
+    get_measures(url_test)
 
 
 if __name__ == "__main__":
