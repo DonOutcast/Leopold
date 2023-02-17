2c7a293c0c3505f7bc666c6680d0abfaebd2fbcd
DonOutcast <lymondgl@student.21-school.ru>
2023-02-17 23:44:50 +0300

    added keyboards

--- src/handlers/register_grants.py
+++ src/handlers/register_grants.py
@@ -5,11 +5,8 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from src.main import bot
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from src.keyboards.markups import get_menu_markup, get_back_menu
 
-
-# from prototype.basicui.keyboards.inline_kb import filter_drop_booking
-# from prototype.basicui.keyboards.inline_kb import city_markup, users_markup
-# from prototype.basicui.keyboards.system_kb import keyboards_menu, back_menu_keyboard
 
 class GrantsStates(StatesGroup):
     name_of_project = State()
@@ -30,4 +27,38 @@ class RegisterGrant:
     def __init__(self, dp):
         self.dp = dp
 
+    @staticmethod
+    async def cmd_start(message: types.Message):
+        path = os.path.abspath('photo/title.jpeg')
+        # m_path = os.path.abspath("1.mp3")
+        with open(path, 'rb') as photo:
+            await bot.send_photo(message.from_user.id,
+                                 photo=photo)
+        # with open(m_path, 'rb') as m_voice:
+        #     await bot.send_voice(message.from_user.id, voice=m_voice, duration=126)
+        await bot.send_message(message.from_user.id, "Добро пожаловать!\nЗдесь вы можете оформить бронь",
+                               reply_markup=get_menu_markup())
+        await message.delete()
 
+        # @staticmethod
+        # async def cmd_cancel_registration(message: types.Message, state: FSMContext):
+        #     await message.delete()
+        #     try:
+        #         await bot.delete_message(message.from_user.id, message_id=message.message_id - 1)
+        #     except:
+        #         pass
+        #     current_state = await state.get_state()
+        #     if current_state is None:
+        #         await message.answer('Вы вернулись в главное меню', reply_markup=keyboards_menu)
+        #         await bot.send_sticker(message.from_user.id,
+        #                                sticker="CAACAgIAAxkBAAENm1Bi_0Q9YClvUdjgvDLx0S5V3Z3UUgAClgcAAmMr4glEcXCvl0uDLSkE")
+        #         return
+        #     await state.finish()
+        #     await message.answer('Вы вернулись в главное меню', reply_markup=keyboards_menu)
+        #     await bot.send_sticker(message.from_user.id,
+        #                            sticker="CAACAgIAAxkBAAENm1Bi_0Q9YClvUdjgvDLx0S5V3Z3UUgAClgcAAmMr4glEcXCvl0uDLSkE")
+
+    def register_handlers_system(self):
+        self.dp.register_message_handler(self.cmd_start, commands=["start"])
+        # self.dp.register_message_handler(self.cmd_cancel_registration, Text(equals="Вернуться в главное меню 📜"),
+        #                                  state="*")
--- src/main.py
+++ src/main.py
@@ -3,16 +3,16 @@ from aiogram import Bot, Dispatcher, types
 from config import API_TOKEN
 from aiogram.contrib.fsm_storage.memory import MemoryStorage  # Позваляет хранить данные в оперативное памяти
 
+storage = MemoryStorage()
+bot = Bot(token=API_TOKEN, parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot, storage=storage)
+
 
 async def on_starttup(_):
     print("Бот запущен!")
 
 
 def start_event_loop():
-    storage = MemoryStorage()
-    # Инициализируем бота
-    bot = Bot(token=API_TOKEN, parse_mode=types.ParseMode.HTML)
-    dp = Dispatcher(bot, storage=storage)
     executor.start_polling(dp, skip_updates=True, on_startup=on_starttup)
 
 
